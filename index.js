0788627e78f9507073bf937f140f9c89aae66cce
Chris Rutherford <therealchrisrutherford@gmail.com>
2019-11-04 16:13:13 -0500

    singleton, inter-model interaction coming soon.

--- index.js
+++ index.js
@@ -1,125 +1,41 @@
-const Keyv = require('keyv');
-const uuid = require('uuid');
+const Model = require('./model');
 const Schema = require('schm');
 
 class Cacheous {
-  constructor({ name, storeURI, schema }) {
-    this.modelCache = new Keyv(storeURI, {
-      namespace: name,
-    });
-    this.modelSchema = schema;
-    this.modelCache.on('error', err => console.error(err));
+  constructor() {
+    modelCache = {};
+    if(!Cacheous.instance){
+      Cacheous.instance = this;
+    }
+    return Cacheous.instance;
   }
 
-  async initIdList() {
-    try {
-      await this.modelCache.set('idList', []);
-    } catch (e) {
-      throw new Error(e);
-    }
+  connect(uri){
+    this.storeURI = uri;
   }
 
-  async validate(model) {
-    try {
-      const res = await this.modelSchema.validate(model);
-      return res;
-    } catch (e) {
-      throw new Error(e[0]);
-    }
+  model({name, schema}) {
+    this.modelCache[name] = {
+      model:  new Keyv(this.storeURI, {
+        namespace: name,
+      }),
+      modelSchema:schema,
+      errorHandler: modelCache[name].on('error', err => console.error(err))
+    };
+    return this.modelCache[name];
   }
 
-  async getIdList() {
-    try {
-      const list = await this.modelCache.get('idList');
-      if (list === undefined) {
-        this.initIdList();
-        return await this.modelCache.get('idList');
-      } else {
-        return list;
-      }
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  async addToIdList(id) {
-    try {
-      const list = await this.getIdList();
-      if (list.indexOf(id) === -1) {
-        await this.modelCache.set('idList', [...list, id]);
-      }
-      return;
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  async getList([...ids]) {
-    console.log(ids);
-    try {
-      const itemList = await ids.map(i => {
-        console.log(i);
-        return this.modelCache.get(i).then(item => {
-          return item;
-        });
-      });
-      return await itemList;
-    } catch (error) {
-      throw new Error(error);
-    }
-  }
-
-  async getById(id) {
-    try {
-      return await this.modelCache.get(id);
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  async where(fnfilter) {
-    try {
-      const idList = await this.getIdList();
-      const schemaList = idList
-        .forEach(async id => {
-          try {
-            return await getById(id);
-          } catch (e) {
-            throw new Error(e);
-          }
-        })
-        .filter(fnFilter);
-      return schemaList;
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  async insert(s) {
-    try {
-      s.id = s.id === undefined ? uuid.v4() : s.id;
-      const result = await this.validate(s);
-      result.id = s.id;
-      await this.modelCache.set(result.id, result);
-      await this.addToIdList(result.id);
-      return result;
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
-
-  async destroy(id) {
-    try {
-      const list = await this.getIdList();
-      await this.modelCache.set('idList', list.filter(x => x !== id));
-      await this.modelCache.delete(id);
-      return true;
-    } catch (e) {
-      throw new Error(e);
-    }
-  }
+ 
 }
 
-Cacheous.Schema = Schema;
+Cacheous = {
+  ...Cacheous,
+  Schema,
+  Model
+};
 
-module.exports = Cacheous;
+const instance = new Cacheous();
+
+Object.freeze(instance);
+
+module.exports = instance;
---
+++ model.js
@@ -0,0 +1,119 @@
+const uuid = require('uuid');
+const Keyv = require('keyv');
+
+module.exports = class Model{
+
+    constructor({name, uri, schema}) {
+        this.modelCache = new Keyv(uri, {namespace: name});
+        this.modelSchema = schema;
+        this.modelCache.on('error', err => console.error(err));
+    }
+
+    async initIdList() {
+        try {
+          await this.modelCache.set('idList', []);
+        } catch (e) {
+          throw new Error(e);
+        }
+      }
+    
+      async validate(model) {
+        try {
+          const res = await this.modelSchema.validate(model);
+          return res;
+        } catch (e) {
+          throw new Error(e[0]);
+        }
+      }
+    
+      async getIdList() {
+        try {
+          const list = await this.modelCache.get('idList');
+          if (list === undefined) {
+            this.initIdList();
+            return await this.modelCache.get('idList');
+          } else {
+            return list;
+          }
+        } catch (e) {
+          throw new Error(e);
+        }
+      }
+    
+      async addToIdList(id) {
+        try {
+          const list = await this.getIdList();
+          if (list.indexOf(id) === -1) {
+            await this.modelCache.set('idList', [...list, id]);
+          }
+          return;
+        } catch (e) {
+          throw new Error(e);
+        }
+      }
+    
+      async getList([...ids]) {
+        console.log(ids);
+        try {
+          const itemList = await ids.map(i => {
+            console.log(i);
+            return this.modelCache.get(i).then(item => {
+              return item;
+            });
+          });
+          return await itemList;
+        } catch (error) {
+          throw new Error(error);
+        }
+      }
+    
+      async getById(id) {
+        try {
+          return await this.modelCache.get(id);
+        } catch (e) {
+          throw new Error(e);
+        }
+      }
+    
+      async where(fnfilter) {
+        try {
+          const idList = await this.getIdList();
+          const schemaList = idList
+            .forEach(async id => {
+              try {
+                return await getById(id);
+              } catch (e) {
+                throw new Error(e);
+              }
+            })
+            .filter(fnFilter);
+          return schemaList;
+        } catch (e) {
+          throw new Error(e);
+        }
+      }
+    
+      async insert(s) {
+        try {
+          s.id = s.id === undefined ? uuid.v4() : s.id;
+          const result = await this.validate(s);
+          result.id = s.id;
+          await this.modelCache.set(result.id, result);
+          await this.addToIdList(result.id);
+          return result;
+        } catch (e) {
+          throw new Error(e);
+        }
+      }
+    
+      async destroy(id) {
+        try {
+          const list = await this.getIdList();
+          await this.modelCache.set('idList', list.filter(x => x !== id));
+          await this.modelCache.delete(id);
+          return true;
+        } catch (e) {
+          throw new Error(e);
+        }
+      }
+}
